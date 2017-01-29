9ecd163af5cd3c155eab96c8b0930479f6ad8ba4
Valkyria <warlaine@hotmail.fr>
2017-01-29 03:15:11 +0100

    Architecture de base Hibernate + .SQL
    Testée sous JDK8
    l'adresse suivante rajoute un utilisateur:
    http://localhost:8080/Projet-conception/Views/factorielle.jsp

---
+++ src/connector_DAO/HibernateSessionFactory.java
@@ -0,0 +1,46 @@
+package connector_DAO;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateSessionFactory {
+
+ private static final SessionFactory sessionFactory;
+
+ static {
+   try {
+   // Crée la SessionFactory
+	   //Configuration configuration = new Configuration().configure();
+	   //StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+	   //applySettings(configuration.getProperties());
+	   //sessionFactory = configuration.buildSessionFactory(builder.build());
+	   sessionFactory = new Configuration().configure().buildSessionFactory();
+   } 
+   catch (HibernateException ex) {
+	   throw new RuntimeException("Problème de configuration : "+ ex.getMessage(), ex);
+   }
+}
+
+ public static final ThreadLocal session = new ThreadLocal();
+
+ public static Session currentSession() throws HibernateException {
+   Session s = (Session) session.get();
+   // Ouvre une nouvelle Session, si ce Thread n'en a aucune
+   if (s == null) {
+	   s = sessionFactory.openSession();
+	   session.set(s);
+   }
+   return s;
+   }
+
+ public static void closeSession() throws HibernateException {
+   Session s = (Session) session.get();
+   session.set(null);
+   if (s != null)
+   s.close();
+   }
+ }
