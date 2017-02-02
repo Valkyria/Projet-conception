fb7a9f3251ce9b571416c71fd873942fec4f7143
cedric <Warlaine@DESKTOP-OCKKBF5>
2017-02-02 17:14:00 +0100

    todo fazia

---
+++ src/controller/loginController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import model_ORM.Utilisateur;
+import connector_DAO.HibernateSessionFactory;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean(name="loginController")
+@RequestScoped
+
+public class loginController {
+	public static void adding_user (String nom){
+    	Session session = HibernateSessionFactory.currentSession();
+    	Transaction tx = session.beginTransaction();
+    	Utilisateur u = new Utilisateur(nom,"fazia","0681813399","104 rue test", "mail@mail.com", "mdp0000");
+    	u.setPrenomUtilisateur(nom);
+    	session.save(u);
+    	tx.commit();
+    	session.close();
+    }
+}
