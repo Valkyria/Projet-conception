52d10d02c1feb2ac48ab7f09afea4535f9441665
Valkyria <warlaine@hotmail.fr>
2017-02-03 11:35:26 +0100

    clearing ctrls

--- src/controller/utilisateurController.java
+++ src/controller/utilisateurController.java
@@ -21,14 +21,6 @@ import services.utilisateurService;
 /* Lien entre la vue et le service*/
 public class utilisateurController 
 {
-	public static void adding_user (String nom){
-    	Session session = HibernateSessionFactory.currentSession();
-    	Transaction tx = session.beginTransaction();
-    	Utilisateur u = new Utilisateur(nom,"fazia","0681813399","104 rue test", "mail@mail.com", "mdp0000");
-    	u.setPrenomUtilisateur(nom);
-    	session.save(u);
-    	tx.commit();
-    	session.close();
-    }
+	
 	
 }
